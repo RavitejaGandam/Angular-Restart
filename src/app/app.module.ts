1d55e7e38d2f2b369a751d10eee761beb9497c75
Raviteja Gandam <gandamraviteja@gmail.com>
2024-06-05 13:06:43 +0530

    basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MainComponent } from './main/main.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent,
+    MainComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
